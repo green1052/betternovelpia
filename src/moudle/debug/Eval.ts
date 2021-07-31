bbb4848b3e228b86a6636e9eec27966d02160914
green1052 <cydyellowgreen@gmail.com>
2021-08-01 07:59:33 +0900

    1

---
+++ src/moudle/debug/Eval.ts
@@ -0,0 +1,28 @@
+import $ from "jquery";
+
+export default {Start};
+
+function Start() {
+    if (!GM_config.get("Eval") || location.pathname.includes("/viewer/"))
+        return;
+
+    const h5 = $("<h5>eval</h5>");
+
+    const li = $("<li>")
+        .css("padding", "10px 25px")
+        .on("click", () => {
+            const code = prompt("code: ");
+
+            if (!code)
+                return;
+
+            try {
+                alert(`결과:\n${eval(code)}`);
+            } catch (e) {
+                alert(`오류:\n${e}`);
+            }
+        })
+        .append(h5);
+
+    $(".am-sideleft > div:nth-child(1) > ul:nth-child(1)").append(li);
+}
