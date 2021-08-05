0a5e3de76375a8e5afd9777a332ba42b8f170e5c
green1052 <cydyellowgreen@gmail.com>
2021-08-06 05:33:47 +0900

    1

--- src/moudle/debug/DisableViewerLog.ts
+++ src/moudle/debug/DisableViewerLog.ts
@@ -4,16 +4,23 @@ function start() {
     if (!GM_config.get("DisableViewLog") || !location.pathname.includes("/viewer/"))
         return;
 
-    unsafeWindow.up_down_btn_view = new Function(unsafeWindow.up_down_btn_view.toString()
+    const upDownBtnView = unsafeWindow.up_down_btn_view.toString()
         .replace(`console.log("up_down_btn_view('" + option + "');");`, "")
         .replace(`console.log("currentPercentage('" + currentPercentage + "');");`, "")
         .replace(`console.log("#scroll_down_btn - show;");`, "")
         .replace(`console.log("#scroll_down_btn - hide;");`, "")
         .replace(`console.log("#scroll_down_btn - show2;");`, "")
-        .replace(`console.log("#scroll_down_btn - hide2;");`, ""));
+        .replace(`console.log("#scroll_down_btn - hide2;");`, "");
 
-    unsafeWindow.navi_view = new Function(unsafeWindow.navi_view.toString()
+    unsafeWindow.up_down_btn_view = (option: "on" | "off") =>
+        eval(`${upDownBtnView}
+        up_down_btn_view(option)`);
+
+    const naviView = unsafeWindow.navi_view.toString()
         .replace("console.log('test_option_btn_comment:' + option_btn_comment);", "")
         .replace("console.log('test_toggle_navi:' + toggle_navi);", "")
-        .replace(`console.log("navi_view - off");`, ""));
+        .replace(`console.log("navi_view - off");`, "");
+
+    unsafeWindow.navi_view = () => eval(`${naviView}
+    navi_view()`);
 }
