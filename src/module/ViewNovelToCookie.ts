5951ee4e14f4ea7536b402ae788103e4e7e5bfd9
green1052 <green1052@pm.me>
2023-04-22 01:58:53 +0900

    10.6.4

--- src/module/ViewNovelToCookie.ts
+++ src/module/ViewNovelToCookie.ts
@@ -32,7 +32,7 @@ export default {
         }
     },
     async start() {
-        const blocked = document.querySelector<HTMLElement>(".one-event-viewer-plus");
+        const blocked = document.querySelector<HTMLElement>(".s_inv:has(.pay-layer-v3)");
 
         if (!blocked)
             return;
