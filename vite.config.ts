701863ccba99e667a28af501a2a4f5929767e87c
green1052 <green1052@pm.me>
2026-08-24 01:47:19 +0900

    chore: update dependencies and configuration for Preact and Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import {defineConfig} from "vite";
 import preact from "@preact/preset-vite";
 import monkey from "vite-plugin-monkey";
-import pkg from "./package.json" with {type: "json"};
 
 export default defineConfig({
+    build: {
+        minify: "oxc"
+    },
     plugins: [
         preact(),
         monkey({
@@ -11,22 +13,18 @@ export default defineConfig({
             userscript: {
                 name: "BetterNovelpia",
                 namespace: "BetterNovelpia",
-                version: pkg.version,
-                author: pkg.author,
                 description: "노벨피아를 더 좋게 바꿔줍니다!",
                 "run-at": "document-start",
                 match: "https://novelpia.com/*",
                 homepageURL: "https://github.com/green1052/betternovelpia",
                 supportURL: "https://github.com/green1052/betternovelpia/issues",
                 downloadURL: "https://github.com/green1052/betternovelpia/releases/latest/download/betternovelpia.user.js",
-                updateURL: "https://github.com/green1052/betternovelpia/releases/latest/download/betternovelpia.meta.js",
+                updateURL: "https://github.com/green1052/betternovelpia/releases/latest/download/betternovelpia.meta.js"
             },
-            server: {mountGmApi: true},
             build: {
                 fileName: "betternovelpia.user.js",
-                metaFileName: "betternovelpia.meta.js",
-            },
-        }),
-    ],
-    build: {minify: true, target: "esnext"},
+                metaFileName: "betternovelpia.meta.js"
+            }
+        })
+    ]
 });
